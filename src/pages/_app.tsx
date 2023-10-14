3d204ccc3e9e8c4ef91af73bb43a274b3b5189af
Tessa-P <tessapugh10@gmail.com>
2023-10-13 22:04:09 -0400

    toast error messages and loading states

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,10 +6,14 @@ import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 // import { Toaster } from "react-hot-toast";
 import Head from "next/head";
+import { Toaster } from "react-hot-toast";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Toaster
+        position="bottom-center"
+      />
       <Component {...pageProps} />
     </ClerkProvider>
   );
